f9a84e12ddd527f946e058d27ba74780aff8a576
ethanketell <ethan@ketell.com>
2018-12-10 21:40:14 -0800

    Added angle label, and sin/cos lines + labels

--- Unit Circle/src/CircleApp.java
+++ Unit Circle/src/CircleApp.java
@@ -1,6 +1,7 @@
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
@@ -9,10 +10,13 @@ import java.awt.Shape;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Area;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.Path2D;
-import java.awt.image.BufferedImage;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -45,6 +49,9 @@ public class CircleApp extends JPanel {
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
+		
+		String text;
+		FontMetrics metrics = g.getFontMetrics();
 
 		double radius = Math.min(getWidth(), getHeight());
 		radius /= 2;
@@ -78,12 +85,44 @@ public class CircleApp extends JPanel {
 		// Unit circle itself
 		g2.draw(new Ellipse2D.Double(-radius, -radius, 2 * radius, 2 * radius));
 
+		// The intersection point
+		Point p = new Point((int)(radius*Math.cos(theta)), (int)(radius*Math.sin(theta)));
+		
 		// Primary radius
-		g2.draw(new Line2D.Double(0, 0, radius * Math.cos(theta), radius * Math.sin(theta)));
+		g2.drawLine(0,0,p.x,p.y);
 
-		// Angle indicator
-		Shape clip = g2.getClip();
+		// Further drawing is dashed
 		g2.setStroke(new BasicStroke(1, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND, 0, new float[] { 2, 3 }, 0));
+		
+		// Sine line
+		g2.setColor(new Color(0xcc33ff));
+		g2.drawLine(0,p.y,p.x,p.y);
+		
+		// Sine label
+		text = String.format("sin(Θ) = %.3f", Math.sin(theta));
+		drawString(g2, text,
+				(Math.abs(theta) < Math.PI/2)?10:-(10+metrics.stringWidth(text)),
+				p.y+5
+				);
+		
+		// Cosine line
+		g2.setColor(new Color(0xff6600));
+		g2.drawLine(p.x,0,p.x,p.y);
+		
+		// Cosine label
+		text = String.format("cos(Θ) = %.3f", Math.cos(theta));
+		drawString(g2, text,
+				p.x+((Math.abs(theta) < Math.PI/2)?5:-(metrics.stringWidth(text)+5)),
+				((Math.signum(theta) == -1)?-(metrics.getAscent()+5+radius/50):5+radius/50)
+				);
+		
+		
+		
+		// Angle indicator partial circle
+		g2.setColor(Color.ORANGE.darker());
+		
+		Shape centerCircle = new Ellipse2D.Double(-radius / 4, -radius / 4, radius / 2, radius / 2);
+		Shape clip = g2.getClip();
 		Path2D thetaClip = new Path2D.Double();
 		thetaClip.moveTo(0, 0);
 		thetaClip.lineTo(radius, 0);
@@ -91,10 +130,30 @@ public class CircleApp extends JPanel {
 		thetaClip.lineTo(radius * Math.cos(theta), radius * Math.sin(theta));
 		thetaClip.closePath();
 		g2.setClip(thetaClip);
-		g2.draw(new Ellipse2D.Double(-radius / 4, -radius / 4, radius / 2, radius / 2));
+		g2.draw(centerCircle);
 		g2.setClip(clip);
-
+		
+		// Angle label
+		text = String.format("%.1f°", Math.toDegrees(theta));
+		Rectangle2D angleBounds = metrics.getStringBounds(text, g2);
+		Area angleArea = new Area(centerCircle);
+		double moveTheta = theta/2;
+		int distance = 0;
+		Area lableArea = new Area(angleBounds);
+		while(!lableArea.isEmpty()) {
+			lableArea.transform(AffineTransform.getTranslateInstance(Math.cos(moveTheta), Math.sin(moveTheta)));
+			lableArea.intersect(angleArea);
+			distance++;
+		}
+		drawString(g2, text, distance*Math.cos(moveTheta), distance*Math.sin(moveTheta));
+		
 		g2.dispose();
+	}
+	
+	private void drawString(Graphics g, String s, double x, double y) {
+		Graphics2D g2 = (Graphics2D)g.create();
+		g2.scale(1, -1);
+		g2.drawString(s,(float)x,(float)-y);
 	}
 
 	private class CircleListener implements MouseListener, MouseMotionListener {
